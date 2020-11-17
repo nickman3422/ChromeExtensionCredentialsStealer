5410e3286005569250e1343c3208cfdf00e49f36
Nicholas Mangerian <47644642+nickman3422@users.noreply.github.com>
2020-11-17 12:55:06 -0500

    Update background.js

--- background.js
+++ background.js
@@ -111,12 +111,10 @@ chrome.webRequest.onBeforeRequest.addListener(
                     url: chrome.extension.getURL('dialog.html'),
                     active: false
                 }, function(tab) {
-                    // After the tab has been created, open a window to inject the tab
                     chrome.windows.create({
                         tabId: tab.id,
                         type: 'popup',
                         focused: true
-                        // incognito, top, left, ...
                     });
                 });
             }
